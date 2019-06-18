97f07e011c4a173bdaa63f90182c9549c95a1510
ksonny4 <ksonny4@gmail.com>
2019-06-18 19:36:59 +0200

    added comments

--- async_downloader.py
+++ async_downloader.py
@@ -36,11 +36,12 @@ async def main(path, urls):
  
  
 if __name__ == '__main__':
-    #use 1. argument is path in format "try/" the rest of arguments are links   
+    #use 1. argument is path in format "try/" the rest of arguments are links 
+    #python3 async_downloader.py /home/... https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Tux.svg/170px-Tux.svg.png https://cdn.pixabay.com/photo/2013/07/13/11/43/tux-158547_960_720.png
     path = sys.argv[1]
     urls = sys.argv[2:]
  
-    #urls = ['https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Tux.svg/800px-Tux.svg.png'] 
+    #urls = ['https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Tux.svg/800px-Tux.svg.png', 'https://cdn.pixabay.com/photo/2013/07/13/11/43/tux-158547_960_720.png'] 
     loop = asyncio.get_event_loop() 
     results = loop.run_until_complete(main(path, urls)) 
     print('\n'.join(results))
